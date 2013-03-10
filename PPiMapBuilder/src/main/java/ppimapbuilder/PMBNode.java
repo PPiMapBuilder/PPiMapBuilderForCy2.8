67d38ef1d59bcb843a328aefda1e3df5ea4e0198
pidupuis <pidupuis@pidupuis-X301A>
2013-03-10 01:48:37 +0100

    Test uniprotJAPI

--- PPiMapBuilder/src/main/java/ppimapbuilder/PMBNode.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/PMBNode.java
@@ -1,5 +1,9 @@
 package ppimapbuilder;
 
+import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
+import uk.ac.ebi.kraken.interfaces.uniref.UniRefEntry;
+import uk.ac.ebi.kraken.uuw.services.remoting.EntryRetrievalService;
+import uk.ac.ebi.kraken.uuw.services.remoting.UniProtJAPI;
 import giny.model.RootGraph;
 import cytoscape.CyNode;
 
@@ -32,6 +36,25 @@ public class PMBNode extends CyNode {
 		
 		this.geneName = geneName;
 		this.uniprotId = uniprotId;
+		
+		//Create entry retrival service
+	    EntryRetrievalService entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
+	    
+	    //Retrieve UniProt entry by its accession number
+	    UniProtEntry entry = (UniProtEntry) entryRetrievalService.getUniProtEntry("Q96AZ6");
+	    
+	    //If entry with a given accession number is not found, entry will be equal null
+	    if (entry != null) {
+	        System.out.println("entry = " + entry.getUniProtId().getValue());
+	    }
+	    
+	    //Retrieve UniRef entry by its ID
+	    UniRefEntry uniRefEntry = entryRetrievalService.getUniRefEntry("UniRef90_Q12979-2");
+	    
+	    if (uniRefEntry != null) {
+	        System.out.println("Representative Member Organism = " +
+	          uniRefEntry.getRepresentativeMember().getSourceOrganism().getValue());
+	    }
 	}
 		
 
