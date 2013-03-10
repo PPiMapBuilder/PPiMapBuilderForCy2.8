222ccb1c838cfd7f4926f42c0280626f31aa7b55
gcornut <gcornut@gmail.com>
2013-03-10 17:16:01 +0100

    Semi-working loading window

--- PPiMapBuilder/src/main/java/ppimapbuilder/gui/LoadingWindow.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/gui/LoadingWindow.java
@@ -2,45 +2,81 @@ package ppimapbuilder.gui;
 
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JPopupMenu;
-import javax.swing.JProgressBar;
 import javax.swing.JLabel;
-import javax.swing.JWindow;
-
-import uk.ac.ebi.kraken.uuw.services.remoting.SetOperation;
-
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Toolkit;
 
-public class LoadingWindow extends JWindow{
+public abstract class LoadingWindow implements Runnable{
 	
-	private static final long serialVersionUID = 1L;
-
+	private JDialog loadingwindow;
+	private JLabel text;
+	
+	private String message;
+	
+	/**
+	 * Create a new loading window with a displayed message
+	 * @param message
+	 */
 	public LoadingWindow(String message) {
-		super();
-		setAlwaysOnTop(true);
-		
-		JLabel spinner =  new JLabel();
-		JLabel text = new JLabel(message);
-		
-		try {
-			spinner.setIcon(new ImageIcon(getClass().getResource("/spinner.gif")));
-		} catch (Exception e) {
-			spinner.setText("Loading...");
-		}
-		
-		getContentPane().add(spinner, BorderLayout.CENTER);
-		getContentPane().add(text, BorderLayout.CENTER);
-		
-		this.pack();
-		
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation(screenSize.width/2 - (getWidth()/2), screenSize.height/2 - (getHeight()/2));
-		screenSize = null;
-		
-		this.setVisible(true);
-		repaint();
+		this.message = message;
+		run();
 	}
+	
+	/**
+	 * Creates the JDialog with spinner 
+	 * @return the dialog window
+	 */
+	private void initialize() {
+		loadingwindow = new JDialog();
+		loadingwindow.setUndecorated(true);
+		
+		// Boder adding a 5px margin
+		EmptyBorder margin = new EmptyBorder(5, 5, 5, 5);
+		
+		text = new JLabel(message);
+		text.setBorder(margin);
+		
+//		JLabel spinner =  new JLabel();
+//		spinner.setBorder(margin);
+//		
+//		ImageIcon gif = null;
+//		try {
+//			gif = new ImageIcon(getClass().getResource("/spinner.gif"));
+//		} catch (Exception e) {}
+//		
+//		spinner.setMinimumSize(new Dimension(gif.getIconWidth(), gif.getIconHeight()));
+//		spinner.setPreferredSize(new Dimension(gif.getIconWidth(), gif.getIconHeight()));
+//		spinner.setMaximumSize(new Dimension(gif.getIconWidth(), gif.getIconHeight()));
+//		spinner.setIcon(gif);
+//		
+//		loadingwindow.getContentPane().add(spinner, BorderLayout.CENTER);
+		
+		loadingwindow.getContentPane().add(text, BorderLayout.CENTER);
+		
+		loadingwindow.pack();
+		loadingwindow.setLocationRelativeTo(null);
+	}
+	
+
+	/**
+	 * When launch the loading window appear, run the treatment and close
+	 */
+	@Override
+	public void run() {
+		initialize();
+
+		loadingwindow.setVisible(true);
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				process();
+				loadingwindow.setVisible(false);
+			}
+		});
+	}
+	
+	public abstract void process();
 }
--- PPiMapBuilder/src/main/java/ppimapbuilder/gui/listener/CreateNetworkFrameSubmitListener.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/gui/listener/CreateNetworkFrameSubmitListener.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 import cytoscape.CyEdge;
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
@@ -15,6 +17,7 @@ import cytoscape.data.Semantics;
 import ppimapbuilder.Mediator;
 import ppimapbuilder.PMBNode;
 import ppimapbuilder.gui.CreateNetworkFrame;
+import ppimapbuilder.gui.LoadingWindow;
 import ppimapbuilder.ppidb.api.DBConnector;
 import ppimapbuilder.ppidb.api.SQLResult;
 
@@ -49,66 +52,65 @@ public class CreateNetworkFrameSubmitListener implements ActionListener{
 			JOptionPane.showMessageDialog(myFrame, "The identifier list is empty.", "", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
-		myFrame.close();
 		
-		//ProgressBarFrame myPBF = new ProgressBarFrame();
-		//myPBF.setVisible(true);
-		
-		
-		//dbList = myFrame.getDatabaseValues(); // Retrieve the database list
-		//orgaList = myFrame.getOrganismValues(); // Retrieve the organism list
-		
-		// Creation of the network
-		myNetwork = Cytoscape.createNetwork("", false); // Creation of a network (with the name of the poi)
+		// Will display the loading window until the treatment is done
+		new LoadingWindow("Generating network...") {
+			public void process() {
+				myFrame.close();
+				
+				//dbList = myFrame.getDatabaseValues(); // Retrieve the database list
+				//orgaList = myFrame.getOrganismValues(); // Retrieve the organism list
+				
+				// Creation of the network
+				myNetwork = Cytoscape.createNetwork("", false); // Creation of a network (with the name of the poi)
 
-		// Creation of the network components
-		for (String id : poiList) { // For each protein of interest
-			SQLResult res;
-			// Get result from 
-			try {
-				res = myDBConnector.getAllData(id);
-				if(res.isEmpty()) throw new SQLException("empty "+id);
-			} catch (SQLException e1) {
-				JOptionPane.showMessageDialog(myFrame, "Error SQL: "+e1.getMessage());
-				return;
-			}
-			
-			PMBNode A, B;
-			CyEdge interaction;
-			LinkedHashMap<String, String> fields;
-			
-			// For each line
-			for(String row: res.keySet()) {
-				//Get fields
-				fields = res.getData(row);
-				
-				//Create Nodes
-				try {
+				// Creation of the network components
+				for (String id : poiList) { // For each protein of interest
+					SQLResult res;
+					// Get result from 
+					try {
+						res = myDBConnector.getAllData(id);
+						if(res.isEmpty()) throw new SQLException("empty "+id);
+					} catch (SQLException e1) {
+						JOptionPane.showMessageDialog(myFrame, "Error SQL: "+e1.getMessage());
+						return;
+					}
 					
-					A = new PMBNode(Cytoscape.getCyNode(fields.get("interactor_nameA"), true), fields.get("uniprotA"));
-					B = new PMBNode(Cytoscape.getCyNode(fields.get("interactor_nameB"), true), fields.get("uniprotB"));
-					myNetwork.addNode(A);
-					myNetwork.addNode(B);
-					myMediator.addNode(A);
-					myMediator.addNode(B);
+					PMBNode A, B;
+					CyEdge interaction;
+					LinkedHashMap<String, String> fields;
 					
-					//Create Edges
-					interaction = Cytoscape.getCyEdge(A, B, Semantics.INTERACTION, "pp", true);
-					myNetwork.addEdge(interaction);
-				
-				} catch (UnknownHostException e1) {
-					e1.printStackTrace();
+					// For each line
+					for(String row: res.keySet()) {
+						//Get fields
+						fields = res.getData(row);
+						
+						//Create Nodes
+						try {
+							
+							A = new PMBNode(Cytoscape.getCyNode(fields.get("interactor_nameA"), true), fields.get("uniprotA"));
+							B = new PMBNode(Cytoscape.getCyNode(fields.get("interactor_nameB"), true), fields.get("uniprotB"));
+							myNetwork.addNode(A);
+							myNetwork.addNode(B);
+							myMediator.addNode(A);
+							myMediator.addNode(B);
+							
+							//Create Edges
+							interaction = Cytoscape.getCyEdge(A, B, Semantics.INTERACTION, "pp", true);
+							myNetwork.addEdge(interaction);
+						
+						} catch (UnknownHostException e1) {
+							e1.printStackTrace();
+						}
+						
+					}
+					
 				}
 				
+				// Add the network to the mediator
+				Mediator.Instance().addNetwork(myNetwork);
 			}
-			
-		}
-		
-		// Add the network to the mediator
-		Mediator.Instance().addNetwork(myNetwork);
-		
-		//myPBF.setVisible(false);
-
+		};
 	}
 		
 }
