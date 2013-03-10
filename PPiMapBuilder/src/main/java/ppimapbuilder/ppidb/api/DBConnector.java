3f4b2f9dfa6394645cc8b31521ddccd452866ab7
keuv <keuv@keuv-305U1A>
2013-03-10 01:02:35 +0100

    SQLResult: now implement all usefull methods to manipulate a SQL result
    DBConnector:
    simplified and minor changes

--- PPiMapBuilder/src/main/java/ppimapbuilder/ppidb/api/DBConnector.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/ppidb/api/DBConnector.java
@@ -2,9 +2,9 @@ package ppimapbuilder.ppidb.api;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
@@ -15,20 +15,39 @@ import java.util.LinkedHashMap;
 public class DBConnector {
 
     private Connection con = null;
-    private Statement st = null;
+    private PreparedStatement pst = null;
     private ResultSet rs = null;
-    private String url = "jdbc:postgresql://jesuisunegrossepatateetjetediszutfacedepot";
+    private String url = "jdbc:postgresql://kgravouil.no-ip.org/ppimapbuilder";
     private String user = "ppimapbuilder";
     private String password = "ppimapbuilder";
 
-    public DBConnector() {
-        try {
-            con = DriverManager.getConnection(this.url, this.user, this.password);
-            st = con.createStatement();
+    
+    
+    public DBConnector() throws SQLException {
 
-        } catch (SQLException ex) {
-            System.err.println("Error while connecting : " + ex.getLocalizedMessage());
-        }
+        con = DriverManager.getConnection(this.url, this.user, this.password);
+        pst = con.prepareStatement(
+                " SELECT DISTINCT"
+                + "     interaction.id AS \"id\","
+                + "     p1.gene_name AS \"interactor_nameA\","
+                + "     p1.uniprot_id AS \"uniprotA\","
+                + "     p2.gene_name AS \"interactor_nameB\","
+                + "     p2.uniprot_id AS \"uniprotB\","
+                + "     org.tax_id AS \"taxID\","
+                + "     db.name AS \"db_source\","
+                + "     expsys.name AS \"exp_system\","
+                + "     pub.pubmed_id as \"pubmed_id\""
+                + " FROM link_data_interaction"
+                + "     JOIN interaction_data ON link_data_interaction.interaction_id = interaction_data.id"
+                + "     JOIN source_database AS \"db\" ON interaction_data.db_source_name = db.name"
+                + "     JOIN organism AS \"org\" ON interaction_data.organism_tax_id = org.tax_id"
+                + "     JOIN experimental_system AS \"expsys\" ON interaction_data.experimental_system = expsys.name"
+                + "     JOIN publication AS \"pub\" ON interaction_data.pubmed_id = pub.pubmed_id"
+                + "     JOIN interaction ON link_data_interaction.interaction_id = interaction.id"
+                + "     JOIN protein AS \"p1\" ON interaction.protein_id1 = p1.id"
+                + "     JOIN protein AS \"p2\" ON interaction.protein_id2 = p2.id"
+                + " WHERE p1.uniprot_id = ?");
+
     }
 
     /**
@@ -39,7 +58,7 @@ public class DBConnector {
      */
     public LinkedHashMap getOrganisms() throws SQLException {
         LinkedHashMap<String, Integer> orga = new LinkedHashMap();
-        rs = st.executeQuery("SELECT tax_id AS \"id\", name AS \"organism\" FROM organism");
+        rs = pst.executeQuery("SELECT tax_id AS \"id\", name AS \"organism\" FROM organism");
 
         while (rs.next()) {
             orga.put(rs.getString("organism"), rs.getInt("id"));
@@ -57,35 +76,39 @@ public class DBConnector {
      */
     public ArrayList getDatabases() throws SQLException {
         ArrayList<String> db = new ArrayList();
-        rs = st.executeQuery("SELECT initcap(name) as \"db\" FROM source_database");
-
+        rs = pst.executeQuery("SELECT initcap(name) as \"db\" FROM source_database");
         while (rs.next()) {
             db.add(rs.getString("db"));
         }
-
         return db;
     }
 
     /**
-     * Get data about a protein identified by its UniprotID and the chosen
-     * reference organism.
+     * Get data about a protein identified by its UniprotID.
      *
      * @param uniprot UniprotID
-     * @param organism Taxonomic ID
      * @return SQLResult
      * @throws SQLExeception
      */
-    public SQLResult getAllData(String uniprot, int organism) throws SQLException {
+    public SQLResult getAllData(String uniprot) throws SQLException {
+        pst.setString(1, uniprot);
+        rs = pst.executeQuery();
+        return new SQLResult(rs);
+    }
+    
+    public ArrayList getKeys(SQLResult sqlr) {
+        return ((ArrayList)sqlr.keySet());
+    }
 
-        //TODO: faire une belle requete des familles pour récuperer toutes les infos de l'uniprot/organism donné.
-
-        String query = "select * from protein limit 5";
-
-
-        rs = st.executeQuery(query);
-        SQLResult sqlr = new SQLResult(rs, "id");
-        return sqlr;
-
+    /**
+     * Get data about a list of proteins identified by their UniprotID.
+     *
+     * @param uniprot UniprotID
+     * @return SQLResult
+     * @throws SQLExeception
+     */
+    public SQLResult getAllData(ArrayList<String> uniprotList) throws SQLException {
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Override
@@ -95,8 +118,8 @@ public class DBConnector {
             if (rs != null) {
                 rs.close();
             }
-            if (st != null) {
-                st.close();
+            if (pst != null) {
+                pst.close();
             }
             if (con != null) {
                 con.close();
