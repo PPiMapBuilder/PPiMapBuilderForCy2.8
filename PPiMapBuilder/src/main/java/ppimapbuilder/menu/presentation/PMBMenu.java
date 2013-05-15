6f63ebd7bbc82eaec4d3cd0ba7c4c1a30d3f5d53
Keuv <Keuv@Keuv-fixe>
2013-05-16 01:48:14 +0200

    Add a menu item to cytoscape to display the changelog window

--- PPiMapBuilder/src/main/java/ppimapbuilder/menu/presentation/PMBMenu.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/menu/presentation/PMBMenu.java
@@ -14,9 +14,10 @@ public class PMBMenu extends JMenu{
 	
 	private static PMBMenu _instance = null; // Instance of the ppimapbuilder menu to prevent several instances 
 	
-	private JMenuItem menuItem_create_network, menuItem_credits; // Menu item to create a new network and to display the credits
+	private JMenuItem menuItem_create_network, menuItem_credits, menuItem_changelog; // Menu item to create a new network and to display the credits
 	private CreateNetworkMenuAction myCreateNetworkAction; // Action to create a network
 	private CreditMenuAction myCreditsAction; // Action to display the credit
+	private ChangelogMenuAction myChangelogAction; // Action to display the credit
 	
 	/**
 	 * Default constructor which is private to prevent several instances
@@ -32,9 +33,14 @@ public class PMBMenu extends JMenu{
 		menuItem_credits = new JMenuItem("About PPiMapBuilder"); // Create the menu item corresponding to the credits display
 		myCreditsAction = new CreditMenuAction(); // Create the action
 		menuItem_credits.addActionListener(myCreditsAction); // Link this action to the menu item
-		
+
+                menuItem_changelog = new JMenuItem("Changelog"); // Create the menu item corresponding to the changelog display
+		myChangelogAction = new ChangelogMenuAction(); // Create the action
+		menuItem_changelog.addActionListener(myChangelogAction); // Link this action to the menu item
+                
 		this.add(menuItem_create_network); // Add the menu item to the main menu
 		this.add(menuItem_credits);
+		this.add(menuItem_changelog);
 	}
 	
 	/**
