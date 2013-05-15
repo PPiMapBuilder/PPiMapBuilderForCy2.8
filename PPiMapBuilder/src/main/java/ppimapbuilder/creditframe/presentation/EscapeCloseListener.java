b9a17c4e5d549931f1bf7a002512139e0d54b0db
Keuv <Keuv@Keuv-fixe>
2013-05-15 23:51:11 +0200

    minor refactoring

--- PPiMapBuilder/src/main/java/ppimapbuilder/creditframe/presentation/EscapeCloseListener.java
+++ PPiMapBuilder/src/main/java/ppimapbuilder/creditframe/presentation/EscapeCloseListener.java
@@ -5,22 +5,23 @@ import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
 /**
- * 
+ *
  * @author CORNUT, CRESSANT, DUPUIS, GRAVOUIL
  *
  */
 public class EscapeCloseListener implements KeyEventDispatcher {
-	
-	private JFrame window;
-	
-	public EscapeCloseListener(JFrame window) {
-		this.window = window;
-	}
-	
-	public boolean dispatchKeyEvent(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-			window.setVisible(false);
-        return false;
-	}
 
+    private JFrame window;
+
+    public EscapeCloseListener(JFrame window) {
+        this.window = window;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            window.setVisible(false);
+        }
+        return false;
+    }
 }
